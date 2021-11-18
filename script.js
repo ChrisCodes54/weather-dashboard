40fef4caf87daef2c3acbfb61f017c6bdcb2a8b7
ChrisCodes54 <christangarife95@gmail.com>
2021-11-18 17:03:42 -0500

    added 5 day forecast

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function getCurrentWeather(citysearch) {
   .then(data => {
     console.log(data)
     let cardTitle = $("<h2 class='card-title>").text(data.name);
-    let temp = $("<p class='card-text'>").text("Temperature: " + data.main.temp + "F");
+    let temp = $("<p class='card-text'>").text("Temperature: " + data.main.temp + " F");
     let humidity =$("<p class='card-text'>").text("Humidity: " + data.main.humidity);
     let wind =$("<p class='card-text'>").text("Wind Speed: " + data.wind.speed);
 
@@ -61,19 +61,42 @@ function getCurrentWeather(citysearch) {
 
   function fiveDayForecast(coords){
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=imperial`
 
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
       console.log("FIVE DAY DATA",data)
+      for(i = 0; i < 5; i++) {
 
+        let weatherContainer = $("#weather-forecast-container")
 
+      // let cardTitle = $("<h2 class='card-title>").text(data.name);
+      let temp = $("<p class='card-text'>").text("Temperature: " + data.daily[i].temp.day + " F");
+      let humidity =$("<p class='card-text'>").text("Humidity: " + data.daily[i].humidity);
+      let wind =$("<p class='card-text'>").text("Wind Speed: " + data.daily[i].wind_speed);
+    
+      let card = $("<div class='card'>")
+      let cardHeader = $("<div class='card-header'>")
+      let cardBody = $("<div class='card-body'>")
+    
+      // cardHeader.append(cardTitle)
+      cardBody.append(temp, humidity, wind)
+      card.append(cardHeader, cardBody)
+      weatherContainer.append(card);
+      }
 
     })
+    // let year = new Date(forecast.dt * 1000).getFullYear();
+
+
+
+
     // in this apiURL you will need to dynamically put in the coords for the call.
     // basically follow the getCurrentWeather for flow.
     // BUT! When getting into dynamically creating elements do it inside of a for loop because you need 5 days
+
+
 
   }
   
